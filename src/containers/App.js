8f23f6c5af85f58be29692d1918d29b1bd41bf56
Souliman Moahmed <medslymn@gmail.com>
2023-05-27 13:27:31 +0100

    commiting changes after finalisation and optimisation

---
+++ src/containers/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css'
+import Scroll from '../components/Scroll';
+
+class App extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            Robots : [],
+            searchField : ''
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(Response=> Response.json())
+        .then(users=> this.setState({Robots:users}))
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value})
+    }
+    
+    render(){
+        const { Robots, searchField } = this.state;
+        const filteredRobots = Robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+        return !Robots.length ?
+            <h1>Loading</h1> :
+           (
+                <div className='tc'>
+                    <h1 className='f1'>RobotFriends</h1>
+                    <SearchBox searchChange = {this.onSearchChange}/>
+                    <Scroll>
+                        <CardList Robots={filteredRobots}/>  
+                    </Scroll> 
+                </div>
+            );
+        }
+    }
+export default App;
